862fe8df5d7f77681a5b0cbdea9f8ebd8ba60214
Alexander Morland <alek@redpill-linpro.com>
2011-07-01 16:09:27 +0200

    Static access methods in Router with 100% coverage

--- extensions/net/socket/Router.php
+++ extensions/net/socket/Router.php
@@ -1,7 +1,38 @@
 <?php
+/**
+ * Li3_Zmq - ZeroMQ plugin for Lithium
+ *
+ * @package       li3_zmq
+ * @copyright     Copyright 2011, Redpill-Linpro (http://redpill-linpro.com)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
 
-namespace li3_zmq\net\socket;
+namespace li3_zmq\extensions\net\socket;
 
+/**
+ * Static class to access Route object
+ */
 class Router extends \lithium\core\Object {
-	
+
+	protected static $_route = null;
+
+	public static function get() {
+		if (static::$_route == null) static::$_route = new \li3_zmq\extensions\net\socket\Route();
+		return static::$_route;
+	}
+
+	public static function process($request) {
+		return static::parse($request);
+	}
+
+	public static function parse($request) {
+		$route = static::get();
+		return $route->parse($request);
+	}
+
+	public static function reset() {
+		static::$_route = new \li3_zmq\extensions\net\socket\Route();
+		return static::$_route;
+	}
+
 }
--- tests/cases/extensions/net/socket/RouterTest.php
+++ tests/cases/extensions/net/socket/RouterTest.php
@@ -1,9 +1,35 @@
 <?php
+/**
+ * Li3_Zmq - ZeroMQ plugin for Lithium
+ *
+ * @package       li3_zmq
+ * @copyright     Copyright 2011, Redpill-Linpro (http://redpill-linpro.com)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
 
 namespace li3_zmq\tests\cases\extensions\net\socket;
 
-use li3_zmq\extensions\net\socket;
+use li3_zmq\extensions\net\socket\Route;
+use li3_zmq\extensions\net\socket\Router;
+
+	ds('echo', true);
 
 class RouterTest extends \lithium\test\Unit {
+
+	public function testMethods() {
+		$this->assertTrue(Router::get() instanceof Route);
+
+		$expected = 'get/posts/1?admin=1';
+		$result = Router::parse($expected)->__toString();
+		$this->assertEqual($expected, $result);
+
+		$expected = array('type' => 'get');
+		$result = Router::reset()->export();
+		$this->assertEqual($expected, $result);
+
+		$expected = array('type' => 'get', 'resource' => 'posts', 'location' => '11');
+		$result = Router::process('get/posts/11')->export();
+		$this->assertEqual($expected, $result);
+	}
 
 }
