72ad1ddaccbfe3629776c421d4884792308afb6e
Alexander Morland <alek@redpill-linpro.com>
2011-07-01 13:40:54 +0200

    added console command for zmq

---
+++ extensions/command/Zmq.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace li3_zmq\extensions\command;
+
+/**
+ * Start or test ZeroMQ sockets and services
+ */
+class Zmq extends \lithium\console\Command {
+	
+	/**
+	 * Start service
+	 */
+	public function service() {
+		$this->out('Service not yet implemented');
+	}
+
+}
