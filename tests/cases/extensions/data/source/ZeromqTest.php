3b21b2b0075b995431c77d1e13bdd49d0d727187
Alexander Morland <alek@redpill-linpro.com>
2011-07-06 11:17:28 +0200

    fixed bad namespace statement

--- tests/cases/extensions/data/source/ZeromqTest.php
+++ tests/cases/extensions/data/source/ZeromqTest.php
@@ -7,7 +7,7 @@
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
-namespace li3_zmq\tests\cases\data\source;
+namespace li3_zmq\tests\cases\extensions\data\source;
 
 use li3_zmq\extensions\data\source\Zeromq;
 
